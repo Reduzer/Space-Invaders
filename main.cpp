498cf38059cc47e4509969420f67c69faa65e3b3
Fyn Deubner <thereduzer897@gmail.com>
2026-03-23 18:16:26 +0100

    Works again
    Keep in mind to use clang++ while building!

--- main.cpp
+++ main.cpp
@@ -2,12 +2,14 @@
 // Created by Fyn Deubner on 19.03.26.
 //
 
-#include <GLFW3/glfw3.h>
+#include <cstdio>
+#include <glew/glew.h>
+#include <glfw/glfw3.h>
 
-GLFWwindow* m_pWindow;
-int m_nHeight = 480;
-int m_nWidth = 640;
-const char* m_pcTitle = "Space Invaders";
+GLFWwindow* window;
+int Height = 480;
+int Width = 640;
+const char* Title = "Space Invaders";
 
 void error_callback(int error, const char* description)
 {
@@ -16,26 +18,45 @@ void error_callback(int error, const char* description)
 
 int main()
 {
-
-    GLFWwindow* window;
-
-
-    /* Initialize the library */
+    //Initialize GLFW
     if (!glfwInit())
         return -1;
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    // Initialize window with specific settings
+    window = glfwCreateWindow(Width, Height, "Space Invaders", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
         return -1;
     }
 
+    //Set OpenGL Version
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
-    /* Loop until the user closes the window */
+    //Initialize GLEW
+    GLenum err = glewInit();
+    if(err != GLEW_OK)
+    {
+        fprintf(stderr, "Error initializing GLEW.\n");
+        glfwTerminate();
+        return -1;
+    }
+
+    int anOpenGLInfo[2] = {-1,1};
+    glGetIntegerv(GL_MAJOR_VERSION, &anOpenGLInfo[0]);
+    glGetIntegerv(GL_MINOR_VERSION, &anOpenGLInfo[1]);
+
+    printf("Using OpenGL: %d.%d\n", anOpenGLInfo[0], anOpenGLInfo[1]);
+
+    glClearColor(1.0, 0.0, 0.0, 1.0);
+
+    // Main Loop for the game
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
@@ -48,6 +69,7 @@ int main()
         glfwPollEvents();
     }
 
+    glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
 }
